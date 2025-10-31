cf2e95618ccda19e3d235c0106482d167d005b6f
Lyubitskiy Maxim <freshentree99@gmail.com>
2025-11-01 04:13:44 +0700

    Add Base Layer class

--- src/neural/include/neural/Layer.hpp
+++ src/neural/include/neural/Layer.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <string>
-#include <vector>
+
+#include "Types.hpp"
 
 namespace neural_net
 {
 
 template <typename T>
+    requires std::floating_point<T>
 class Layer
 {
 private:
@@ -15,7 +17,31 @@ public:
 
     virtual void initialize(int input_size) = 0;
 
-    virtual std::vector<T> forward(const std::vector<T>)
+    virtual Tensor<T> forward(const Tensor<T> &input) = 0;
+
+    virtual Tensor<T> backwadr(const Tensor<T> &gradient, T learning_rate) = 0;
+
+    virtual int get_output_size() const = 0;
+
+    virtual std::string get_name() const = 0;
+
+    virtual Matrix<T> get_weights() const
+    {
+        return {};
+    }
+
+    virtual Tensor<T> get_biases() const
+    {
+        return {};
+    }
+
+    virtual void set_weights(const Matrix<T> &weights)
+    {
+    }
+
+    virtual void set_biases(const Tensor<T> &biases)
+    {
+    }
 };
 
 } // namespace neural_net
