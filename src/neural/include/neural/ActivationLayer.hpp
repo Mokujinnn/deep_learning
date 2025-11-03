8ce6f10dee7f1246df3331fc0f27cd9247870bcb
Lyubitskiy Maxim <freshentree99@gmail.com>
2025-11-03 08:29:24 +0700

    Add ActivationLayer class

---
+++ src/neural/include/neural/ActivationLayer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Layer.hpp"
+
+namespace neural_net
+{
+
+namespace layers
+{
+
+template <std::floating_point T>
+class ActivationLayer
+{
+private:
+    /* data */
+public:
+    ActivationLayer(/* args */);
+    ~ActivationLayer();
+};
+
+} // namespace layers
+
+} // namespace neural_net
